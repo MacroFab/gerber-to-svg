534cd2b88c2b80b44791a1e8347f806692de6c35
metacollin <canderson@macrofab.com>
2024-11-04 13:30:18 -0700

    intial commit (#4)
    Co-authored-by: metacollin <metacollin@electropi.mp>

--- debian/gerber-to-svg.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the gerber-to-svg package
-#
-0 4	* * *	root	[ -x /usr/bin/gerber-to-svg_maintenance ] && /usr/bin/gerber-to-svg_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(gerber-to-svg):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="gerber-to-svg" command="/usr/bin/gerber-to-svg"
